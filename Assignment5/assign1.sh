a562089abbfbf97deebf942f7da0015e29df2c3c
Shweta91002 <shwetapatil91002@gmail.com>
2024-10-11 13:34:40 +0530

    assignment

---
+++ Assignment5/assign1.sh
@@ -0,0 +1,8 @@
+echo -n "Home Directory : "
+pwd
+
+echo -n "Login name : "
+whoami
+
+echo  "Home Directory : $HOME"
+echo  "Login name : $USER"
---
+++ Assignment5/assign2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -n "Date : "
+date
+echo -n "Calender : "
+cal
+echo -n "Directory Files : "
+ls
+echo -n "Path : "
+pwd
+echo -n "Bye...!exit"
+exit
+
+
